1d2689f106cc3654166de771124723484f25a7c4
Junhong <raimonfuns@gmail.com>
2019-08-22 12:16:46 +0800

    feat: add cars anyway server

--- fe/src/api.ts
+++ fe/src/api.ts
@@ -3,7 +3,7 @@ import Parser from 'rss-parser';
 export default {
   fetchFeed(rssUrl: string) {
     const parser = new Parser();
-    const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
-    return parser.parseURL(CORS_PROXY + rssUrl);
+    const { REACT_APP_CORS_PROXY } = process.env;
+    return parser.parseURL(REACT_APP_CORS_PROXY + rssUrl);
   },
 };
---
+++ start-be.sh
@@ -0,0 +1,1 @@
+cd be && yarn start
