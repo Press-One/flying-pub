9110bf46b76128e3ffd122f1c039c104b77427b3
Junhong <raimonfuns@gmail.com>
2019-09-10 17:29:58 +0800

    tweak: remove cars anyway

--- fe/src/api.ts
+++ fe/src/api.ts
@@ -3,7 +3,6 @@ import Parser from 'rss-parser';
 export default {
   fetchFeed(rssUrl: string) {
     const parser = new Parser();
-    const { REACT_APP_CORS_PROXY } = process.env;
-    return parser.parseURL(REACT_APP_CORS_PROXY + rssUrl);
+    return parser.parseURL(rssUrl);
   },
 };
