f6a58abfc584e3785c7e2a43e58dd0e5a5142650
Junhong <raimonfuns@gmail.com>
2019-09-17 23:08:40 +0800

    tweak: use different tag for medium

--- build_cn.sh
+++ build_cn.sh
@@ -5,8 +5,11 @@ git checkout release
 git clean -f -d
 git pull
 
-IMAGE_NAME="dh-cn.press.one/pressone/medium"
+BOX_IMAGE_NAME="dh-cn.press.one/pressone/medium:box"
+XUE_IMAGE_NAME="dh-cn.press.one/pressone/medium:xue"
 
 sudo docker login --username pressone --password 57e348ab37aa5b55f68b7642ac584a41 dh-cn.press.one
-sudo docker build -f Dockerfile-production -t $IMAGE_NAME .
-sudo docker push $IMAGE_NAME
+sudo docker build -f Dockerfile-production -t $BOX_IMAGE_NAME .
+sudo docker tag $(docker images | awk '{print $3}' | awk 'NR==2') $XUE_IMAGE_NAME
+sudo docker push $BOX_IMAGE_NAME
+sudo docker push $XUE_IMAGE_NAME
