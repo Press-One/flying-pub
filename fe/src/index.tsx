2ebc2012c02ccdf020b228e70408e3da625af1c4
Junhong <raimonfuns@gmail.com>
2019-09-17 23:25:28 +0800

    trigger pipeline

--- fe/src/index.tsx
+++ fe/src/index.tsx
@@ -10,7 +10,6 @@ if (process.env.NODE_ENV === 'production') {
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// 测试专用: 2019-09-17 11:15
