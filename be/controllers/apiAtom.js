a28a5ecb22af50ffc92ffa852cbdfe028dd48f8e
Junhong <raimonfuns@gmail.com>
2019-11-08 10:49:02 +0800

    tweak: posts config

--- be/controllers/apiAtom.js
+++ be/controllers/apiAtom.js
@@ -3,8 +3,7 @@ const request = require('request-promise');
 
 exports.get = async (ctx) => {
   const atom = await request({
-    // uri: `https://xue-posts.press.one/output/${config.topicAddress}`
-    uri: `https://xue-posts.xue.cn/output/b6b17424f87ffb8b5b853291f6dbaf0aac661ca2`
+    uri: `${config.serviceRoot}/output/${config.topicAddress}`
   }).promise();
   ctx.body = atom;
 }
