aa9db0583fcadd58d476804478d244390ed1b999
Junhong <raimonfuns@gmail.com>
2019-11-17 09:02:46 +0800

    tweak: posts

--- fe/src/pages/Post/index.tsx
+++ fe/src/pages/Post/index.tsx
@@ -184,7 +184,7 @@ export default observer((props: any) => {
           )}
         </div>
         {!noReward && <RewardSummary summary={rewardSummary} />}
-        {!noReward && <div className="mt-8 pb-4 md:pb-10 border-t border-gray-300" />}
+        {!noReward && <div className="mt-8 pb-4 md:border-none border-t border-gray-300" />}
       </div>
     );
   };
