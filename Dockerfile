76435f1eae935076ba78dfb55f8b24557f9ccf49
Junhong <raimonfuns@gmail.com>
2019-11-19 17:24:04 +0800

    tweak: posts

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:10.15.2
 
 ADD . /app
-ADD ./fe/build /app/be/build
+RUN mv ./fe/build /app/be/build
 
 WORKDIR /app/be
 RUN npm config set registry https://registry.npm.taobao.org
--- build_cn.sh
+++ build_cn.sh
@@ -14,5 +14,4 @@ IMAGE_NAME="dh-cn.press.one/pressone/medium"
 
 sudo docker login --username pressone --password 57e348ab37aa5b55f68b7642ac584a41 dh-cn.press.one
 sudo docker build -t $IMAGE_NAME .
-
 sudo docker push $IMAGE_NAME
