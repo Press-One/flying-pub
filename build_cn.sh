de71fe42d304aacfd5c65f68a07c768a7108704b
Junhong <raimonfuns@gmail.com>
2019-11-19 16:34:01 +0800

    tweak: config

--- build_cn.sh
+++ build_cn.sh
@@ -13,4 +13,5 @@ IMAGE_NAME="dh-cn.press.one/pressone/medium"
 
 sudo docker login --username pressone --password 57e348ab37aa5b55f68b7642ac584a41 dh-cn.press.one
 sudo docker build -f Dockerfile-production -t $IMAGE_NAME .
+
 sudo docker push $IMAGE_NAME
