e8e2606aece19ce825310809be27899fba86f866
Junhong <raimonfuns@gmail.com>
2019-11-26 23:23:03 +0800

    tweaks(common): remove comment order by createdAt

--- be/models/comment.js
+++ be/models/comment.js
@@ -1,33 +1,27 @@
-const {
-  assert,
-  Errors
-} = require('./validator');
-const Comment = require('./sequelize/comment');
-const User = require('./user');
-const Post = require('./post');
-const Vote = require('./vote');
+const { assert, Errors } = require("./validator");
+const Comment = require("./sequelize/comment");
+const User = require("./user");
+const Post = require("./post");
+const Vote = require("./vote");
 
 const packComment = async (comment, options = {}) => {
-  const {
-    userId
-  } = options;
-  assert(comment, Errors.ERR_NOT_FOUND('comment'));
+  const { userId } = options;
+  assert(comment, Errors.ERR_NOT_FOUND("comment"));
   const commentJson = comment.toJSON();
   const user = await User.get(commentJson.userId, {
     withProfile: true
   });
   commentJson.user = user;
-  const voted = !!userId && await Vote.isVoted(userId, 'comments', comment.id);
+  const voted =
+    !!userId && (await Vote.isVoted(userId, "comments", comment.id));
   commentJson.voted = voted;
   delete commentJson.deleted;
   return commentJson;
-}
+};
 
 exports.get = async (id, options = {}) => {
-  const {
-    userId
-  } = options;
-  assert(id, Errors.ERR_IS_REQUIRED('id'));
+  const { userId } = options;
+  assert(id, Errors.ERR_IS_REQUIRED("id"));
   const comment = await Comment.findOne({
     where: {
       id,
@@ -41,11 +35,11 @@ exports.get = async (id, options = {}) => {
 };
 
 exports.create = async (userId, data) => {
-  assert(userId, Errors.ERR_IS_REQUIRED('userId'));
-  assert(data, Errors.ERR_IS_REQUIRED('data'));
+  assert(userId, Errors.ERR_IS_REQUIRED("userId"));
+  assert(data, Errors.ERR_IS_REQUIRED("data"));
   const payload = {
     ...data,
-    userId,
+    userId
   };
   const comment = await Comment.create(payload);
   const derivedComment = await packComment(comment, {
@@ -54,75 +48,71 @@ exports.create = async (userId, data) => {
   const fileRId = derivedComment.objectId;
   const count = await exports.count(fileRId);
   await Post.upsert(fileRId, {
-    commentsCount: count,
+    commentsCount: count
   });
   return derivedComment;
 };
 
 exports.update = async (id, data) => {
-  assert(id, Errors.ERR_IS_REQUIRED('commentId'));
-  assert(data, Errors.ERR_IS_REQUIRED('data'));
+  assert(id, Errors.ERR_IS_REQUIRED("commentId"));
+  assert(data, Errors.ERR_IS_REQUIRED("data"));
   await Comment.update(data, {
     where: {
       id
     }
-  })
+  });
   return true;
 };
 
 exports.delete = async id => {
-  assert(id, Errors.ERR_IS_REQUIRED('id'));
+  assert(id, Errors.ERR_IS_REQUIRED("id"));
   const derivedComment = await exports.get(id);
   const fileRId = derivedComment.objectId;
-  await Comment.update({
-    deleted: true
-  }, {
-    where: {
-      id
+  await Comment.update(
+    {
+      deleted: true
+    },
+    {
+      where: {
+        id
+      }
     }
-  });
+  );
   const count = await exports.count(fileRId);
   await Post.upsert(fileRId, {
-    commentsCount: count,
+    commentsCount: count
   });
   return true;
 };
 
-exports.count = async (objectId) => {
-  assert(objectId, Errors.ERR_IS_REQUIRED('objectId'));
+exports.count = async objectId => {
+  assert(objectId, Errors.ERR_IS_REQUIRED("objectId"));
   const count = await Comment.count({
     where: {
       objectId,
-      deleted: false,
-    },
+      deleted: false
+    }
   });
   return count;
 };
 
-exports.list = async (options) => {
-  const {
-    userId,
-    objectId,
-    offset,
-    limit,
-  } = options;
+exports.list = async options => {
+  const { userId, objectId, offset, limit } = options;
   const comments = await Comment.findAll({
     where: {
       objectId,
-      deleted: false,
+      deleted: false
     },
     offset,
     limit,
-    // order: [
-    //   ['upVotesCount', 'DESC']
-    // ]
+    order: [["createdAt", "ASC"]]
   });
   const list = await Promise.all(
-    comments.map((comment) => {
+    comments.map(comment => {
       return packComment(comment, {
         userId
       });
     })
-  )
+  );
   return list;
 };
