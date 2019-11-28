1f636792bcb44b4428afbcdc742d0973edcfd85b
Junhong <raimonfuns@gmail.com>
2019-11-28 08:07:34 +0800

    tweaks(common): receipt log

--- be/models/finance.js
+++ be/models/finance.js
@@ -353,7 +353,7 @@ const updateReceiptByUuid = async (uuid, data) => {
   const updatedReceipt = await Receipt.getByUuid(uuid);
   Log.createAnonymity(
     "更新收据",
-    JSON.stringify(updatedReceipt.id)
+    updatedReceipt.id
   );
   const user = await User.getByAddress(updatedReceipt.fromAddress);
   if (updatedReceipt.type === "RECHARGE") {
@@ -446,7 +446,7 @@ const syncInitializedReceipt = async receipt => {
         const timeoutReceipt = await Receipt.getByUuid(receipt.uuid);
         Log.createAnonymity(
           "这条收据过期了，状态已设置为 TIMEOUT",
-          `${JSON.stringify(timeoutReceipt.id)}`
+          timeoutReceipt.id
         );
       } else {
         log(`${thisReceipt.id} 处于 ${minutes} 分钟等待期`);
