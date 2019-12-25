747da53ae4feb3ab67a83d23e348057e854a505b
Junhong <raimonfuns@gmail.com>
2019-12-25 18:45:42 +0800

    tweak: pub config

--- build_cn.sh
+++ build_cn.sh
@@ -2,9 +2,12 @@ cd fe
 npm config set registry https://registry.npm.taobao.org
 yarn install
 yarn build
-cd /pressone/pub
+cd /pressone/medium
 
 IMAGE_NAME="dh-cn.press.one/pressone/medium"
+BOX_IMAGE_NAME="dh-cn.press.one/pressone/box-medium"
+XUE_IMAGE_NAME="dh-cn.press.one/pressone/xue-medium"
+PRS_IMAGE_NAME="dh-cn.press.one/pressone/prs-medium"
 
 sudo docker login --username pressone --password 57e348ab37aa5b55f68b7642ac584a41 dh-cn.press.one
 sudo docker build -t $IMAGE_NAME .
