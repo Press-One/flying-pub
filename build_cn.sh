71ac2effecaf084f83ae33c06733d6b0095b6043
Junhong <raimonfuns@gmail.com>
2019-12-30 11:34:53 +0800

    tweak: xue config

--- build_cn.sh
+++ build_cn.sh
@@ -6,7 +6,7 @@ cd /pressone/medium
 
 IMAGE_NAME="dh-cn.press.one/pressone/medium"
 # BOX_IMAGE_NAME="dh-cn.press.one/pressone/box-medium"
-# XUE_IMAGE_NAME="dh-cn.press.one/pressone/xue-medium"
+XUE_IMAGE_NAME="dh-cn.press.one/pressone/xue-medium"
 PRS_IMAGE_NAME="dh-cn.press.one/pressone/prs-medium"
 
 sudo docker login --username pressone --password 57e348ab37aa5b55f68b7642ac584a41 dh-cn.press.one
@@ -17,6 +17,6 @@ sudo docker build -t $IMAGE_NAME .
 sudo docker tag $IMAGE_NAME $PRS_IMAGE_NAME
 
 # sudo docker push $BOX_IMAGE_NAME
-# sudo docker push $XUE_IMAGE_NAME
+sudo docker push $XUE_IMAGE_NAME
 sudo docker push $PRS_IMAGE_NAME
 
