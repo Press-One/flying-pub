b08c93dcf118981ab01d28a0253c23d5b851387c
Junhong <raimonfuns@gmail.com>
2020-06-17 09:55:26 +0800

    tweak(flying-pub): config

--- build_cn.sh
+++ build_cn.sh
@@ -6,13 +6,13 @@ npm config set registry https://registry.npm.taobao.org
 yarn install && yarn build || { exit 1; }
 cd /pressone/reader
 
-IMAGE_NAME="dh-cn.press.one/pressone/reader"
-BOX_IMAGE_NAME="dh-cn.press.one/pressone/box-reader"
-XUE_IMAGE_NAME="dh-cn.press.one/pressone/xue-reader"
-PRS_IMAGE_NAME="dh-cn.press.one/pressone/prs-reader"
-BLOG_IMAGE_NAME="dh-cn.press.one/pressone/blog-reader"
+IMAGE_NAME="dockerhub.qingcloud.com/pressone/reader"
+BOX_IMAGE_NAME="dockerhub.qingcloud.com/pressone/box-reader"
+XUE_IMAGE_NAME="dockerhub.qingcloud.com/pressone/xue-reader"
+PRS_IMAGE_NAME="dockerhub.qingcloud.com/pressone/prs-reader"
+BLOG_IMAGE_NAME="dockerhub.qingcloud.com/pressone/blog-reader"
 
-sudo docker login --username pressone --password 57e348ab37aa5b55f68b7642ac584a41 dh-cn.press.one
+sudo docker login -u prs-admin -p 57e348ab37aa5b55f68b7642ac584a41 dockerhub.qingcloud.com
 sudo docker build -t $IMAGE_NAME .
 
 sudo docker tag $IMAGE_NAME $BOX_IMAGE_NAME
