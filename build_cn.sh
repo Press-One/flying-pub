9d7ab15ce6f44944ab646cc834f62356ac806019
Junhong <raimonfuns@gmail.com>
2020-07-09 10:01:54 +0800

    feat(folder): support folder param

--- build_cn.sh
+++ build_cn.sh
@@ -10,8 +10,8 @@ cd /pressone/reader
 sudo docker login -u prs-admin -p 57e348ab37aa5b55f68b7642ac584a41 dockerhub.qingcloud.com
 
 # 静态文件上传到 CDN
-docker run -it --rm -v $(pwd)/client/build/static/js:/app/src dockerhub.qingcloud.com/pressone/qingcloud-uploader sh -c "npm start -- --folder='reader/static/js'"
-docker run -it --rm -v $(pwd)/client/build/static/css:/app/src dockerhub.qingcloud.com/pressone/qingcloud-uploader sh -c "npm start -- --folder='reader/static/css'"
+sudo docker run -it --rm -v $(pwd)/client/build/static/js:/app/src dockerhub.qingcloud.com/pressone/qingcloud-uploader sh -c "npm start -- --folder='reader/static/js'"
+sudo docker run -it --rm -v $(pwd)/client/build/static/css:/app/src dockerhub.qingcloud.com/pressone/qingcloud-uploader sh -c "npm start -- --folder='reader/static/css'"
 
 IMAGE_NAME="dockerhub.qingcloud.com/pressone/reader"
 BOX_IMAGE_NAME="dockerhub.qingcloud.com/pressone/box-reader"
