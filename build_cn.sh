0bee861ee2f44b028eca920346c345c5a0073189
Junhong <raimonfuns@gmail.com>
2020-08-06 11:34:34 +0800

    tweak(build): push image to new repo

--- build_cn.sh
+++ build_cn.sh
@@ -9,8 +9,8 @@ cd /pressone/reader
 sudo docker login -u prs-admin -p 57e348ab37aa5b55f68b7642ac584a41 dockerhub.qingcloud.com
 
 # 静态文件上传到 CDN
-sudo docker run --rm -v $(pwd)/client/build/static/js:/app/src dockerhub.qingcloud.com/pressone/qingcloud-uploader sh -c "npm start -- --folder='reader/static/js'"
-sudo docker run --rm -v $(pwd)/client/build/static/css:/app/src dockerhub.qingcloud.com/pressone/qingcloud-uploader sh -c "npm start -- --folder='reader/static/css'"
+sudo docker run --rm -v $(pwd)/client/build/static/js:/app/src dockerhub.qingcloud.com/pressone_private/qingcloud-uploader sh -c "npm start -- --folder='reader/static/js'"
+sudo docker run --rm -v $(pwd)/client/build/static/css:/app/src dockerhub.qingcloud.com/pressone_private/qingcloud-uploader sh -c "npm start -- --folder='reader/static/css'"
 
 IMAGE_NAME="dockerhub.qingcloud.com/pressone/reader"
 BOX_IMAGE_NAME="dockerhub.qingcloud.com/pressone/box-reader"
