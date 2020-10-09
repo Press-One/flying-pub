e9767ae92190e464e86af65c7a4c318ac5c0b4dc
Junhong <raimonfuns@gmail.com>
2020-10-09 17:21:14 +0800

    tweak(build): only push box image

--- build_cn.sh
+++ build_cn.sh
@@ -13,22 +13,22 @@ sudo docker run --rm -v $(pwd)/client/build/static/js:/app/src dockerhub.qingclo
 sudo docker run --rm -v $(pwd)/client/build/static/css:/app/src dockerhub.qingcloud.com/pressone_private/qingcloud-uploader sh -c "npm start -- --folder='reader/static/css'"
 
 IMAGE_NAME="dockerhub.qingcloud.com/pressone/reader"
-BOX_IMAGE_NAME="dockerhub.qingcloud.com/pressone/box-reader"
-XUE_IMAGE_NAME="dockerhub.qingcloud.com/pressone/xue-reader"
-PRS_IMAGE_NAME="dockerhub.qingcloud.com/pressone/prs-reader"
-BLOG_IMAGE_NAME="dockerhub.qingcloud.com/pressone/blog-reader"
-WRITING_IMAGE_NAME="dockerhub.qingcloud.com/pressone/writing-reader"
+BOX_IMAGE_NAME="dockerhub.qingcloud.com/pressone/flying-pub-box"
+# XUE_IMAGE_NAME="dockerhub.qingcloud.com/pressone/xue-reader"
+# PRS_IMAGE_NAME="dockerhub.qingcloud.com/pressone/prs-reader"
+# BLOG_IMAGE_NAME="dockerhub.qingcloud.com/pressone/blog-reader"
+# WRITING_IMAGE_NAME="dockerhub.qingcloud.com/pressone/writing-reader"
 
 sudo docker build -t $IMAGE_NAME .
 
 sudo docker tag $IMAGE_NAME $BOX_IMAGE_NAME
-sudo docker tag $IMAGE_NAME $XUE_IMAGE_NAME
-sudo docker tag $IMAGE_NAME $PRS_IMAGE_NAME
-sudo docker tag $IMAGE_NAME $BLOG_IMAGE_NAME
-sudo docker tag $IMAGE_NAME $WRITING_IMAGE_NAME
+# sudo docker tag $IMAGE_NAME $XUE_IMAGE_NAME
+# sudo docker tag $IMAGE_NAME $PRS_IMAGE_NAME
+# sudo docker tag $IMAGE_NAME $BLOG_IMAGE_NAME
+# sudo docker tag $IMAGE_NAME $WRITING_IMAGE_NAME
 
 sudo docker push $BOX_IMAGE_NAME
-sudo docker push $XUE_IMAGE_NAME
-sudo docker push $PRS_IMAGE_NAME
-sudo docker push $BLOG_IMAGE_NAME
-sudo docker push $WRITING_IMAGE_NAME
+# sudo docker push $XUE_IMAGE_NAME
+# sudo docker push $PRS_IMAGE_NAME
+# sudo docker push $BLOG_IMAGE_NAME
+# sudo docker push $WRITING_IMAGE_NAME
