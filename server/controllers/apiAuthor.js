6c1d12de799894ff6120ae39d8112d19b9c1ad71
Junhong <raimonfuns@gmail.com>
2020-10-10 13:23:33 +0800

    feat(author): read profile from user if not exist

--- server/controllers/apiAuthor.js
+++ server/controllers/apiAuthor.js
@@ -1,4 +1,5 @@
 const Author = require('../models/author');
+const User = require("../models/user");
 const {
   assert,
   Errors
@@ -8,5 +9,11 @@ exports.get = async ctx => {
   const address = ctx.params.id;
   const author = await Author.getByAddress(address);
   assert(author, Errors.ERR_NOT_FOUND('author'))
+  const authorUser = await User.getByAddress(address);
+  if (authorUser) {
+    author.avatar = author.avatar || authorUser.avatar;
+    author.name = author.name || authorUser.nickname;
+    author.bio = author.bio || authorUser.bio;
+  }
   ctx.body = author;
 }
