b3fb35e8bfd947e3684601a18acb6d0c2c237767
Junhong <raimonfuns@gmail.com>
2020-10-10 13:56:20 +0800

    tweak(build): can build writing image

--- build_cn.sh
+++ build_cn.sh
@@ -14,21 +14,21 @@ sudo docker run --rm -v $(pwd)/client/build/static/css:/app/src dockerhub.qingcl
 
 IMAGE_NAME="dockerhub.qingcloud.com/pressone/reader"
 BOX_IMAGE_NAME="dockerhub.qingcloud.com/pressone/flying-pub-box"
+WRITING_IMAGE_NAME="dockerhub.qingcloud.com/pressone/flying-pub-writing"
 # XUE_IMAGE_NAME="dockerhub.qingcloud.com/pressone/xue-reader"
 # PRS_IMAGE_NAME="dockerhub.qingcloud.com/pressone/prs-reader"
 # BLOG_IMAGE_NAME="dockerhub.qingcloud.com/pressone/blog-reader"
-# WRITING_IMAGE_NAME="dockerhub.qingcloud.com/pressone/writing-reader"
 
 sudo docker build -t $IMAGE_NAME .
 
 sudo docker tag $IMAGE_NAME $BOX_IMAGE_NAME
+sudo docker tag $IMAGE_NAME $WRITING_IMAGE_NAME
 # sudo docker tag $IMAGE_NAME $XUE_IMAGE_NAME
 # sudo docker tag $IMAGE_NAME $PRS_IMAGE_NAME
 # sudo docker tag $IMAGE_NAME $BLOG_IMAGE_NAME
-# sudo docker tag $IMAGE_NAME $WRITING_IMAGE_NAME
 
 sudo docker push $BOX_IMAGE_NAME
+sudo docker push $WRITING_IMAGE_NAME
 # sudo docker push $XUE_IMAGE_NAME
 # sudo docker push $PRS_IMAGE_NAME
 # sudo docker push $BLOG_IMAGE_NAME
-# sudo docker push $WRITING_IMAGE_NAME
