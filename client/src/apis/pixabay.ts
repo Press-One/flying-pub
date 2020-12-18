b21cdc1328d2d3a8fa1a170ecde1ad4637ecdc08
Junhong <raimonfuns@gmail.com>
2020-12-18 18:58:57 +0800

    Revert "tweak(pixabay): tweak request"
    This reverts commit e00482b339b3b8ccdab1a2fc502a2ad32536b79c.

--- client/src/apis/pixabay.ts
+++ client/src/apis/pixabay.ts
@@ -1,8 +1,11 @@
+import request from '../request';
 import qs from 'query-string';
 
 export default {
-  async search(options: any = {}) {
-    const url = `https://pixabay.com/api?key=13927481-1de5dcccace42d9447c90346f&safesearch=true&image_type=photo&${qs.stringify(options)}`;
-    return await (await fetch(url)).json()
+  search(options: any = {}) {
+    return request(`/api?key=13927481-1de5dcccace42d9447c90346f&safesearch=true&image_type=photo&${qs.stringify(options)}`, {
+      base: `https://pixabay.com`,
+      minPendingDuration: 300
+    });
   },
 }
