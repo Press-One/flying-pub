0f0211b5ba4d099e14109df1a614dc658479adad
Junhong <raimonfuns@gmail.com>
2021-03-17 18:09:37 +0800

    tweak: config

--- server/config.example/config.js
+++ server/config.example/config.js
@@ -63,6 +63,7 @@ module.exports = {
       authorizationURL: `https://mixin-www.zeromesh.net/oauth/authorize`,
       tokenURL: 'https://mixin-api.zeromesh.net/oauth/token',
       userProfileURL: 'https://mixin-api.zeromesh.net/me',
+      apiDomain: 'https://mixin-api.zeromesh.net',
       wsDomain: 'wss://mixin-blaze.zeromesh.net/',
       callbackUrl: `${serviceRoot}/api/auth/mixin/callback`
     }
